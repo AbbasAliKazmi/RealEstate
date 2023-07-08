2a23ed236862452488db43d10be1c42a87fd8399
Bapernafrek <syedabbasali1998.aa@gmail.com>
2023-07-08 06:06:21 +0500

    yyes

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import  './GetStarted.css'
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+           <div className="flexColCenter inner-container">
+              <span className='primaryText'>Get Started With Homyz</span>
+              <span className='secondaryText'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              <br/>Optio inventore maiores mollitia?</span>
+
+              <button className='button '>
+                <a href="mailto:syedabbasali1998.aa@gmail.com">Get Started</a>
+              </button>
+              
+           </div>
+        </div>
+    </section>
+  )
+}
+
+export default GetStarted
